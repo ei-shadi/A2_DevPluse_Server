78b9686c0e1b14b108a84451be0381cde47bc12b
ei-shadi <eftajulislam.saadi@gmail.com>
2026-05-22 11:32:48 +0600

    Global Error Handler & Utility based Send Response File Added

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express, { type Application, type Request, type Response } from 'express'
+import globalErrorHandler from './middleware/globalErrorHandler';
 
 const app: Application = express();
 
-
+app.use(express.json());
+app.use(globalErrorHandler);
 
 
 export default app;
